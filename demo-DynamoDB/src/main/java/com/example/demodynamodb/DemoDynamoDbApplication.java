64d34524b77a258180f8418334e28a32881ab0e8
Emma Paris <eparis@atlassian.com>
2018-04-11 14:58:25 +1000

    commit my first changes to git local

---
+++ demo-DynamoDB/src/main/java/com/example/demodynamodb/DemoDynamoDbApplication.java
@@ -0,0 +1,12 @@
+package com.example.demodynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDynamoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDynamoDbApplication.class, args);
+	}
+}

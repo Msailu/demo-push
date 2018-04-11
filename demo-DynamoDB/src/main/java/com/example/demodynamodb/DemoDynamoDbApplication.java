d046a1572c150dfa2c4f139c1de225f9dfaad864
Emma Paris <eparis@atlassian.com>
2018-04-11 15:04:07 +1000

    commit second change to local repo

--- demo-DynamoDB/src/main/java/com/example/demodynamodb/DemoDynamoDbApplication.java
+++ demo-DynamoDB/src/main/java/com/example/demodynamodb/DemoDynamoDbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoDynamoDbApplication {
 
 	public static void main(String[] args) {
+		/** this is sample file **/
 		SpringApplication.run(DemoDynamoDbApplication.class, args);
 	}
 }
---
+++ demo-DynamoDB/src/main/java/com/example/demodynamodb/Test.java
@@ -0,0 +1,5 @@
+package com.example.demodynamodb;
+
+public class Test {
+
+}
